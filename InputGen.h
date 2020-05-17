5e29b5328eaa4de344b87b04bdaddff31bcad750
G K Ajeyaa <ajeyaano.1@gmail.com>
2020-05-17 07:57:45 +0530

    Update InputGen.h

--- InputGen.h
+++ InputGen.h
@@ -9,10 +9,14 @@ class InputGen
     
     public:
         
-        //constructor to initialize the variable test_number to 0
+        //constructor to initialize the variable test_number to 1
         InputGen();
     
-        /*function to generate 1 test file each for cpu and gpu (in order to benchmark against the same input)
-        for particular values of m and n, and returns the name of the file*/
-        std::string genFileInput(int m,int n);
+        /*function to generate 1 test file for particular values of m, n and the computation device used,
+        and returns the name of the file*/
+        std::string genFileInput(int m,int n,bool ifCpuOrGpu);
+    
+        /*function to copy the contents of a given file and just create a new file with the same input
+        but with a different computation device*/
+        std::string switchedComputationFile(std::string filename);
 };
