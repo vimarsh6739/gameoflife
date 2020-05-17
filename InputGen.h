35830a7400d4b98c8700bd0f916f17c8ca09c1b6
G K Ajeyaa <ajeyaano.1@gmail.com>
2020-05-17 06:53:21 +0530

    Update InputGen.h

--- InputGen.h
+++ InputGen.h
@@ -2,5 +2,17 @@
 
 class InputGen
 {
-
+    private:
+        
+        //variable to hold the number of files created earlier in order to avoid naming clashes
+        int test_number;
+    
+    public:
+        
+        //constructor to initialize the variable test_number to 0
+        InputGen();
+    
+        /*function to generate 1 test file each for cpu and gpu (in order to benchmark against the same input)
+        for particular values of m and n*/
+        bool genFileInput(int m,int n);
 };
